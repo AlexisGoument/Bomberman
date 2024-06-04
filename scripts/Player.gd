2caf28e41398ddbf91592a79997a9507e101a14d
Alexis GOUMENT <alexis.nguyen.goument@gmail.com>
2024-06-04 23:41:49 +0200

    dropping a bomb disable it's collision, which reactivate after going out

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,14 +5,8 @@ const SPEED = 300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_dropped_bomb = false
-var dropped_bomb
 
 func _physics_process(delta):
-	if has_dropped_bomb && not player_is_on_bomb():
-		dropped_bomb.get_node("CollisionShape2D").disabled = false
-		has_dropped_bomb = false
-	
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("direction_left", "direction_right")
 	if direction:
@@ -32,14 +26,6 @@ func _input(event):
 		drop_bomb(self.position)
 
 func drop_bomb(coords: Vector2i):
-	if !has_dropped_bomb:
-		has_dropped_bomb = true
-		dropped_bomb = bomb_scene.instantiate();
-		dropped_bomb.position = coords
-		get_parent().add_child(dropped_bomb)
-
-func player_is_on_bomb():
-	return (self.position.x >= dropped_bomb.position.x 
-	and self.position.x < dropped_bomb.position.x + 16
-	and self.position.y >= dropped_bomb.position.y
-	and self.position.y < dropped_bomb.position.y + 16)
+	var dropped_bomb = bomb_scene.instantiate();
+	dropped_bomb.position = coords
+	get_parent().add_child(dropped_bomb)
--- scripts/bomb.gd
+++ scripts/bomb.gd
@@ -4,11 +4,16 @@ extends StaticBody2D
 @onready var ray_cast_left = $Bomb/RayCastLeft
 @onready var ray_cast_right = $Bomb/RayCastRight
 @onready var tilemap: TileMap = $"../TileMap"
+@onready var collision_shape_2d = $CollisionShape2D
 
 const TILESIZE = 16
 const WALL_ATLAS_COORDS = Vector2i(2, 6)
 const WALL_BONUS_ATLAS_COORDS = Vector2i(2, 5)
 var bombLength = 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
 
 func explode():
 	ray_cast_left.target_position.x = bombLength * TILESIZE * -1
@@ -33,3 +38,7 @@ func explode_body(rid: RID):
 	elif atlas_coords == WALL_BONUS_ATLAS_COORDS:
 		tilemap.erase_cell(0, coords)
 		get_parent().create_powerup(coords)
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		collision_shape_2d.set_deferred("disabled", false)
